f1d338cceed5a1fede767b0c82081020a4b8acc5
victor-oli1999 <victoroliveira2016@icloud.com>
2022-10-07 18:37:09 -0300

    Primeira parte da montagem da pagina statica
    Nessa fase somente inseri o JSX e estilizei utilizando o React, nada difícil.

---
+++ React.js/Static Page/index.js
@@ -0,0 +1,17 @@
+const page = (
+    <div>
+        <img src="./react-logo.png" style={{width: "40px"}}></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by jordan Walke</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
+console.log(div)
+
