cc79b3cb72f7e0330d39fdd65c5c9de1477f72d3
victor-oli1999 <victoroliveira2016@icloud.com>
2022-10-11 20:56:47 -0300

    Estilizando a página com CSS
    Aprendi como utilizar o CSS para estilizar a página criada com React.

--- React.js/Static Page/index.js
+++ React.js/Static Page/index.js
@@ -1,17 +1,49 @@
-const page = (
-    <div>
-        <img src="./react-logo.png" style={{width: "40px"}}></img>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+function Header() {
+    return (
+        <header>
+            <nav className="nav-bar">
+                <img src="./react-logo.png" className="logo" ></img>
+                <ul className="nav-items">
+                    <li className="nav-li">Preço</li>
+                    <li className="nav-li">Sobre</li>
+                    <li className="nav-li">Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+function MainContent() {
+    return (
+        <div className="content">
+            <h1>Razões para aprender React.js</h1>
+            <ol>
+                <li>Ganhar dinheiro;</li>
+                <li>Começar a trabalhar de verdade;</li>
+                <li>Não parece ser difícil.</li>
+            </ol>
+        </div>
+    )
+}
+function Footer() {
+    return (
+        <footer className="footer">
+            <small>@ 2022 Oliveira Development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div className="body">
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+ReactDOM.render(
+        <Page />,
+    document.getElementById('root')
 )
 
-ReactDOM.render(page, document.getElementById("root"))
-console.log(div)
 
